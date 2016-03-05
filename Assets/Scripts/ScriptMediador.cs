c97ecc4cfbca7842dc32f465a8543835ec490d96
Denilson Junior <jundenilson@gmail.com>
2016-03-04 23:55:08 -0300

    Primeiro commit com mecânica de pontuação já conceitualmente implementada

---
+++ Assets/Scripts/ScriptMediador.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptMediador : MonoBehaviour {
+    [SerializeField]
+    GameObject botao;
+    [SerializeField]
+    GameObject circulo;
+    [SerializeField]
+    AnimationClip animacao;
+
+    float time = 0;
+    float oldtime;
+    public float intervalo;
+    GameObject instanciado;
+    GameObject oldInstanciado;
+
+    //[SerializeField]
+    // Use this for initialization
+    void Start () {
+        time = 0;
+        oldtime = 0;
+        oldInstanciado = null;
+        //Instantiate(circulo);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (time > oldtime + intervalo) //show de gambiarras
+        {
+            oldInstanciado = instanciado;
+            instanciado = Instantiate(circulo);
+            instanciado.GetComponent<Animation>().AddClip(animacao, "nome que eu quiser");
+            oldtime = time;
+            Destroy(oldInstanciado);
+        }
+        else
+        {
+            time = time + Time.deltaTime;
+        }
+    }
+
+    public void apertarBotao()
+    {
+        Debug.Log("Ponto = " + (1-instanciado.transform.localScale.x));
+        Destroy(instanciado); 
+    }
+}
---
+++ Assets/Scripts/getScale.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class getScale : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //GetComponent<Animation>().
+            //GetComponent<AnimationClip>().frameRate = Random.Range(20.0f, 45.0f);
+        GetComponent<Animation>().Play();
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //sDebug.Log("Tamanho = " + transform.localScale);
+	}
+}
